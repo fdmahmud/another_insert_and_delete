bf2c73a8694d0f6ee0dc14fac3ab56cda76174aa
fdmahmud <ferdousmahmud.pro@gmail.com>
2019-07-22 00:52:19 +0600

    adding some new files 2.

--- index.php
+++ index.php
@@ -11,25 +11,25 @@
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="ex: simple@wow.com">
+                        <input name="email" type="email" class="form-control" id="inputEmail4" placeholder="ex: simple@wow.com">
                     </div>
                     <div class="form-group col-md-6">
                         <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="1 to 16 words">
+                        <input name="password" type="password" class="form-control" id="inputPassword4" placeholder="1 to 16 words">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                    <input name="address1" type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
                 </div>
                 <div class="form-group">
                     <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                    <input name="address2" type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity">
+                        <input type="text" name="city" class="form-control" id="inputCity">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="inputState">State</label>
